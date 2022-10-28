4f51cac144a41b22f1cad916b5cf0c0b18b84f6b
SelenaNovelis <106102142+SelenaNovelis@users.noreply.github.com>
2022-10-27 22:34:40 -0400

    add baseline python file

---
+++ baseline_test.py
@@ -0,0 +1,148 @@
+import os
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.utils
+from tqdm import tqdm
+from utils import get_dataset, get_network, get_eval_pool, evaluate_synset, get_time, DiffAugment, ParamDiffAug
+import wandb
+import copy
+import random
+from reparam_module import ReparamModule
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+
+
+def main(args):
+
+	args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+	args.dsa_param = ParamDiffAug()
+
+	channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader, loader_train_dict, class_map, class_map_inv = get_dataset(args.dataset, args.data_path, args.batch_real, args.subset, args=args)
+
+	images_all = []
+	labels_all = []
+	indices_class = [[] for c in range(num_classes)]
+	print("BUILDING DATASET")
+	for i in tqdm(range(len(dst_train))):
+		sample = dst_train[i]
+		images_all.append(torch.unsqueeze(sample[0], dim=0))
+		labels_all.append(class_map[torch.tensor(sample[1]).item()])
+
+	for i, lab in tqdm(enumerate(labels_all)):
+		indices_class[lab].append(i)
+	images_all = torch.cat(images_all, dim=0).to("cpu")
+	labels_all = torch.tensor(labels_all, dtype=torch.long, device="cpu")
+
+	def get_images(c, n):  # get random n images from class c
+		idx_shuffle = np.random.permutation(indices_class[c])[:n]
+		return images_all[idx_shuffle]
+
+	label_syn = torch.tensor([np.ones(args.ipc)*i for i in range(num_classes)], dtype=torch.long, requires_grad=False, device=args.device).view(-1)
+	image_syn = torch.randn(size=(num_classes * args.ipc, channel, im_size[0], im_size[1]), dtype=torch.float)
+
+	for c in range(num_classes):
+		image_syn.data[c * args.ipc:(c + 1) * args.ipc] = get_images(c, args.ipc).detach().data
+
+	# eval_it_pool = np.arange(0, args.Iteration + 1, args.eval_it).tolist()
+	model_eval_pool = get_eval_pool(args.eval_mode, args.model, args.model)
+	syn_lr = torch.tensor(args.lr_teacher).to(args.device)
+
+
+	for model_eval in model_eval_pool:
+
+		accs_test = []
+		accs_train = []
+		for it_eval in range(args.num_eval):
+			net_eval = get_network(model_eval, channel, num_classes, im_size).to(args.device) # get a random model
+
+			eval_labs = label_syn
+			with torch.no_grad():
+				image_save = image_syn
+			image_syn_eval, label_syn_eval = copy.deepcopy(image_save.detach()), copy.deepcopy(eval_labs.detach()) # avoid any unaware modification
+
+			args.lr_net = syn_lr.item()
+			_, acc_train, acc_test = evaluate_synset(it_eval, net_eval, image_syn_eval, label_syn_eval, testloader, args, texture=args.texture)
+			accs_test.append(acc_test)
+			accs_train.append(acc_train)
+		accs_test = np.array(accs_test)
+		accs_train = np.array(accs_train)
+		acc_test_mean = np.mean(accs_test)
+		acc_test_std = np.std(accs_test)
+
+		print('Evaluate %d random %s, test acc mean = %.4f std = %.4f\n-------------------------'%(len(accs_test), model_eval, acc_test_mean, acc_test_std))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parameter Processing')
+
+    parser.add_argument('--dataset', type=str, default='CIFAR10', help='dataset')
+
+    parser.add_argument('--subset', type=str, default='imagenette', help='ImageNet subset. This only does anything when --dataset=ImageNet')
+
+    parser.add_argument('--model', type=str, default='ConvNet', help='model')
+
+    parser.add_argument('--ipc', type=int, default=1, help='image(s) per class')
+
+    parser.add_argument('--eval_mode', type=str, default='S',
+                        help='eval_mode, check utils.py for more info')
+
+    parser.add_argument('--num_eval', type=int, default=5, help='how many networks to evaluate on')
+
+    parser.add_argument('--eval_it', type=int, default=100, help='how often to evaluate')
+
+    parser.add_argument('--epoch_eval_train', type=int, default=1000, help='epochs to train a model with synthetic data')
+    parser.add_argument('--Iteration', type=int, default=5000, help='how many distillation steps to perform')
+
+    parser.add_argument('--lr_img', type=float, default=1000, help='learning rate for updating synthetic images')
+    parser.add_argument('--lr_lr', type=float, default=1e-05, help='learning rate for updating... learning rate')
+    parser.add_argument('--lr_teacher', type=float, default=0.01, help='initialization for synthetic learning rate')
+
+    parser.add_argument('--lr_init', type=float, default=0.01, help='how to init lr (alpha)')
+
+    parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
+    parser.add_argument('--batch_syn', type=int, default=None, help='should only use this if you run out of VRAM')
+    parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
+
+    parser.add_argument('--pix_init', type=str, default='real', choices=["noise", "real"],
+                        help='noise/real: initialize synthetic images from random noise or randomly sampled real images.')
+
+    parser.add_argument('--dsa', type=str, default='True', choices=['True', 'False'],
+                        help='whether to use differentiable Siamese augmentation.')
+
+    parser.add_argument('--dsa_strategy', type=str, default='color_crop_cutout_flip_scale_rotate',
+                        help='differentiable Siamese augmentation strategy')
+
+    parser.add_argument('--data_path', type=str, default='data', help='dataset path')
+    parser.add_argument('--buffer_path', type=str, default='./buffers', help='buffer path')
+
+    parser.add_argument('--expert_epochs', type=int, default=3, help='how many expert epochs the target params are')
+    parser.add_argument('--syn_steps', type=int, default=20, help='how many steps to take on synthetic data')
+    parser.add_argument('--max_start_epoch', type=int, default=25, help='max epoch we can start at')
+
+    parser.add_argument('--zca', action='store_true', help="do ZCA whitening")
+
+    parser.add_argument('--load_all', action='store_true', help="only use if you can fit all expert trajectories into RAM")
+
+    parser.add_argument('--no_aug', type=bool, default=False, help='this turns off diff aug during distillation')
+
+    parser.add_argument('--texture', action='store_true', help="will distill textures instead")
+    parser.add_argument('--canvas_size', type=int, default=2, help='size of synthetic canvas')
+    parser.add_argument('--canvas_samples', type=int, default=1, help='number of canvas samples per iteration')
+
+
+    parser.add_argument('--max_files', type=int, default=None, help='number of expert files to read (leave as None unless doing ablations)')
+    parser.add_argument('--max_experts', type=int, default=None, help='number of experts to read per file (leave as None unless doing ablations)')
+
+    parser.add_argument('--force_save', action='store_true', help='this will save images for 50ipc')
+
+    args = parser.parse_args()
+
+    main(args)
+
