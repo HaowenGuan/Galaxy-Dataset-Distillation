cc8c27ca26b959b28696152c8c29537aefab0868
Haowen Guan <little.seazer@gmail.com>
2023-04-13 03:29:55 -0400

    Update gzoo2 dataset and utils

--- utils.py
+++ utils.py
@@ -19,6 +19,7 @@ from astropy.table import Table
 import cv2 as cv
 from PIL import Image
 from sklearn.metrics import confusion_matrix
+import gzoo2_dataset
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -58,66 +59,43 @@ def get_dataset(dataset, data_path, batch_size=1, subset="imagenette", args=None
     loader_train_dict = None
     class_map_inv = None
 
-    if dataset == 'gzoo2_normal':
+    if dataset == 'GZoo2':
         channel = 3
         im_size = (128, 128)
         num_classes = 10
 
-        #0.0592 #
-        mean = [0.0376, 0.0331, 0.0248]
-        #0.1058 #
-        std = [0.0754, 0.0617, 0.0547]
+        mean = [0.0735, 0.0600, 0.0482]
+        std = [0.1279, 0.0992, 0.0892]
 
-        gzoo = fits.open(os.path.join('Galaxy-DR17-dataset/gzoo2', 'zoo2MainSpecz_sizes.fit'))[1].data
-        indexes = dict()
-        for i, id in enumerate(gzoo['dr7objid']):
-            indexes[id] = i
+        path = "/data/sbcaesar/classes/6000"
+        if 'gzoo_dataset.pt' not in os.listdir(path):
+            gzoo2_dataset.build(path)
 
-        def get_classes(id):
-            d = gzoo[indexes[id]]
-            class_1 = d['t01_smooth_or_features_a01_smooth_fraction'] * d['t07_rounded_a16_completely_round_fraction']
-            class_2 = d['t01_smooth_or_features_a01_smooth_fraction'] * d['t07_rounded_a17_in_between_fraction']
-            class_3 = d['t01_smooth_or_features_a01_smooth_fraction'] * d['t07_rounded_a18_cigar_shaped_fraction']
-            class_4 = d['t01_smooth_or_features_a02_features_or_disk_fraction'] * d['t02_edgeon_a04_yes_fraction'] * (d['t09_bulge_shape_a25_rounded_fraction'] + d['t09_bulge_shape_a26_boxy_fraction'])
-            class_5 = d['t01_smooth_or_features_a02_features_or_disk_fraction'] * d['t02_edgeon_a04_yes_fraction'] * d['t09_bulge_shape_a27_no_bulge_fraction']
-            class_6 = d['t01_smooth_or_features_a02_features_or_disk_fraction'] * d['t02_edgeon_a05_no_fraction'] * d['t03_bar_a06_bar_fraction'] * d['t04_spiral_a08_spiral_fraction']
-            class_7 = d['t01_smooth_or_features_a02_features_or_disk_fraction'] * d['t02_edgeon_a05_no_fraction'] * d['t03_bar_a06_bar_fraction'] * d['t04_spiral_a09_no_spiral_fraction']
-            class_8 = d['t01_smooth_or_features_a02_features_or_disk_fraction'] * d['t02_edgeon_a05_no_fraction'] * d['t03_bar_a07_no_bar_fraction'] * d['t04_spiral_a08_spiral_fraction']
-            class_9 = d['t01_smooth_or_features_a02_features_or_disk_fraction'] * d['t02_edgeon_a05_no_fraction'] * d['t03_bar_a07_no_bar_fraction'] * d['t04_spiral_a09_no_spiral_fraction']
-            class_10 = d['t01_smooth_or_features_a03_star_or_artifact_fraction']
-
-            classes_l = [class_1, class_2, class_3, class_4, class_5, class_6, class_7, class_8, class_9, class_10]
-            # return np.array(classes_l)
-            return np.argmax(np.array(classes_l))
-
-        path = '/data/sbcaesar/image'
-        dst_total = []
-        count = 0
-        for image in os.listdir(path):
-            if ".jpg" not in image:
-                continue
-            image_dir = os.path.join(path, image)
-            count += 1
-            if count % 1000 == 0: print(count)
-            if count > 10000:
-                break
-
-            id = int(image[:-4])
-            img = cv.imread(image_dir)
-            img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-            img = img[img.shape[0] // 4:(img.shape[0] * 3) // 4, img.shape[1] // 4:(img.shape[1] * 3) // 4]
-            img = cv.resize(img, (128, 128), interpolation=cv.INTER_AREA) / 255
-            img = torch.from_numpy(img.T)
-            img = transforms.Normalize(mean, std)(img)
-
-            dst_total.append((img, get_classes(id)))
-
-        np.random.shuffle(dst_total)
-        dst_train = []
-        dst_test = dst_total
+        gzoo_dataset = torch.load(os.path.join(path, "gzoo_dataset.pt"))
+        dst_train = gzoo_dataset.train
+        dst_test = gzoo_dataset.test
 
         class_names = [str(i) for i in range(num_classes)]
         class_map = {x:x for x in range(num_classes)}
+
+    elif dataset == 'GZoo2_aug':
+        channel = 3
+        im_size = (128, 128)
+        num_classes = 10
+
+        mean = [0.0735, 0.0600, 0.0482]
+        std = [0.1279, 0.0992, 0.0892]
+
+        path = "/data/sbcaesar/classes/6000"
+        if 'gzoo_dataset_aug.pt' not in os.listdir(path):
+            gzoo2_dataset.build(path, aug=10)
+
+        gzoo_dataset = torch.load(os.path.join(path, "gzoo_dataset_aug.pt"))
+        dst_train = gzoo_dataset.train
+        dst_test = gzoo_dataset.test
+
+        class_names = [str(i) for i in range(num_classes)]
+        class_map = {x: x for x in range(num_classes)}
 
     elif dataset == 'gzoo2':
         channel = 3
